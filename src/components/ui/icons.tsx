cd1e02a04f44a452a4b341012f4ff00f7606d745
Prince <dadaprince011@gmail.com>
2026-01-03 15:51:27 +0000

    it is a jpeg file

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -4,7 +4,7 @@ export const Icons = {
   logo: (props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) => (
     <Image
       {...props}
-      src="/logo/logo.png"
+      src="/logo/logo.jpeg"
       alt="Radiant Means Ltd. Logo"
       width={props.width ?? 32}
       height={props.height ?? 32}
