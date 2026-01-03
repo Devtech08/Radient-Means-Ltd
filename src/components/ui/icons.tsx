886fdfdc55130475ecc0369dce105b151a420d0d
Prince <dadaprince011@gmail.com>
2026-01-03 15:49:51 +0000

    i have added the image in the logo folder inn the project folder

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -4,7 +4,7 @@ export const Icons = {
   logo: (props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) => (
     <Image
       {...props}
-      src="/logo.png"
+      src="/logo/logo.png"
       alt="Radiant Means Ltd. Logo"
       width={props.width ?? 32}
       height={props.height ?? 32}
